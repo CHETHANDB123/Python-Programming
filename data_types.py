7af9ce15d376b635899d30c61d6dd483c4a2567c
CHETHAN D B <chethandb.cse@gmail.com>
2026-07-15 20:54:58 +0530

    Single Data Types

---
+++ data_types.py
@@ -0,0 +1,15 @@
+a=22 # initailaization
+address=id(a)
+print(address)  #integer form address
+
+hex_addr=hex(address) #return hexadecimal represent of integer
+print(hex_addr)
+
+print("-------")
+c=0
+print(c)
+datatype=type(c) #type(object)-> datatype of integer
+print(datatype)
+
+y=int()   #Default if int datatype
+print(type(y))
