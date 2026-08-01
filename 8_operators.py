dcbde44f52916f65834e277663dc1ab27bd2b17a
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-01 21:15:09 +0530

    Arithmetic,Assignment ,Relational ,Logical ,Membership

---
+++ 8_operators.py
@@ -0,0 +1,135 @@
+# Opeartor
+# Arithemaic Opeartor
+'''
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/2)
+print(5//2) #floor Division|Qoutient
+print(10%2) # Reminder
+print(2**5) # square root
+print(10**0.5) #100 q
+
+
+# Assignment operator
+a=10
+print(a)
+z=3
+z+=5
+print(z)
+b=9
+b-=5
+print(b)
+c=3
+c*=5
+print(c)
+d=3
+d/=4
+print(d)
+e=4
+e//=5
+print(e)
+r=0
+r%=8
+print(r)
+h=0
+h**=1
+print(h)
+
+#relational operator
+print(11<2)
+print(20>1)
+print(4<=4)
+print(33>=22)
+print(3==3.0)
+print(5!=5)
+a=100
+b=99
+print(a!=b)
+
+#logical operator
+#and,or,not->they usually boolean values
+print(3>2 and 2<3)
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or False)
+print(True or True)
+print(False or False)
+print(False or True)
+
+print(not False)
+print(not True)
+
+print(5 and 4)
+print(0.0 and 5.4)
+print([] and "hi")
+print("0" and "0.0")
+
+print(5 or 4)
+print(5 or {})
+print(5 or 0)
+print(0.0 or 5.4)
+print([] or "hi")
+print("0" or "0.0")
+print({} or set())
+
+#identity operator
+#is,is not
+#it returns a boolean value
+a=5
+b=a
+print(a is not b)
+l1=[10,20,30]
+l2=l1
+print(l1 is l2)
+l3=[10,20,30]
+print(l1 is l3) #memory loc comparision
+
+print(l1==l3) #equality of values
+
+#Membership operator
+#in , not in
+# returns boolean values
+h=[11,22,3,3]
+print(10 in h)
+print(11 in h)
+print(22 not in h)
+
+h1={1:11,2:22,3:3.3}
+print(1 in h1)
+print(11 in h1.values())
+
+h2="chethan www"
+print("z" in h2)
+print(" " not in h2)
+'''
+#Bitwise operator
+#bitwise And op(&)->it compares 2 bits and return 1 when both bits are 1
+#bitwise OR op(|)->compare 2 bits and returns 1 if atleast bit is 1
+#bitwise XOR op(^)-> compare 2  bits and return 1 if both bits are different
+#bitwise NOT op(~)->it inverts the bits ~int value
+#bitwise leftshift op(<<)->*
+#bitwise rightshift op(>>)->/
+a=12
+print(12 & 5)
+print(12|5)
+print(12^5)
+print(~12) # -(a+1)
+print(a<<2)
+print(a>>2)
+
+
+
+
+
+
+
+
+
+
+
+
+
