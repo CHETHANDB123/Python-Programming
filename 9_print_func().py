fc614948aee8918998de0fd4fd061d0c3792bd8d
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-02 23:53:37 +0530

    print func( )

---
+++ 9_print_func().py
@@ -0,0 +1,17 @@
+#print func()
+print(1)
+print(2)
+print(3)
+
+print(1,2,3,4,5) #space is used a separator b/w the values
+#sep
+print(1,2,3,4,5,sep="abc") #only str or none in the palace of separator
+#end
+print(1,2,3,4,end="abs")
+print(2, end="@")
+print(1)
+
+print(1,2,3,4,5,sep="\n",end=" ")
+print(6,7,8,sep=" ",end="")
+print(12,end="")
+print(11)
