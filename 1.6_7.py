e8f5c2966b9bad3f2fc5a3658beb3d2beac516c3
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-16 19:44:58 +0530

    For | While Loop

---
+++ 1.6_7.py
@@ -0,0 +1,111 @@
+#LOOPS
+#for
+'''
+l=[10,20,30,40]
+for i in l: #collection
+    print("Hello")
+    
+
+for i in range(1,18): #range
+    print("Bye")
+   
+l=[1,2,3,4]
+for i in l:
+    print(i)
+    
+for i in range(1,18):
+    print("Bye")
+    
+l=[10,20,30,40,50,60,70,80,90,100]
+for i in range(-1,-10,-2):
+    print(l[i])
+'''
+
+#while
+'''
+a=0 #initalization
+while a<5: #condition
+    print("Hello") 
+    a+=1 #updation
+
+i=1
+while i<6:
+    print(i)
+    i+=1
+
+b=1
+while b<14:
+    print(b)
+    b+=3
+
+c=13
+while c>0:
+    print(c)
+    c+=-3
+
+l=[10,20,30,40,50,60,70,80,90,100]
+i=1 #index
+while i<10:
+    print(l[i])
+    i+=2
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
