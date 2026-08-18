dc424ac31ba920c49208e689e4c03829ded4af1e
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-18 21:31:09 +0530

    All Arguments Codes

---
+++ 1.8_1.py
@@ -0,0 +1,6 @@
+def order_food(hotel_name,*starters,drink="water",**maincourse):
+    print(hotel_name)
+    print(starters)
+    print(drink)
+    print(maincourse)
+order_food("paradise","panner","drygobi","mushroom",drink="coke",roti='butternoon',gravy='kajumasala',rice='gheerice',desert='jamun')
